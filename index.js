158c48702655a37e17e639232b129e2b8488dfd2
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-03 21:27:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function longShout(string){
+  console.log(string.toLowerCase)
+}
+
