4f948a90d1ab4686ca2f71d756a8318415d10da2
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-03 21:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ function whisper(string) {
 function logShout(string){
   console.log(string.toUpperCase())
 }
-
-
-
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 function sayHiGrandmw(string){
-  
+  if (string.toLowerCase === string){
+    
+  }
 }
 
 
